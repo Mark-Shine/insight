2146b18055e3aae724fe212b2b6b4cc197401597
Shiyi <wb-shangzhihai@taobao.com>
2014-07-14 14:08:09 +0800

    修复quote utf-8错误

--- monitor/context_processors.py
+++ monitor/context_processors.py
@@ -15,7 +15,7 @@ def common(request):
     context = dict(
         USER_NAME=user.username,
         TEAM=team,
-        # TEAM_CHANNEL=quote(team),
+        TEAM_CHANNEL=quote(team.encode('utf-8')),
         app="",
         is_admin=is_admin,
         )
